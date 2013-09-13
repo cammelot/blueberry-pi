c95f6452169ab0f440af85d0eab22b30b51b2e98
seonggwang.gwon <g.passcode@gmail.com>
2013-09-14 02:49:55 +0900

    테스트용 비행기체와 립모션 연동 스크립트 추가

---
+++ Assets/Scripts/TestLeapMotion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLeapMotion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LeapInput.HandUpdated += HandUpdatedHandler;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LeapInput.Update();
+	}
+
+    void HandUpdatedHandler(Leap.Hand hand)
+    {
+        Debug.Log(hand.PalmPosition.ToString());
+
+        Leap.Vector newPos = hand.PalmPosition;
+        transform.position = new Vector3(newPos.x * 0.05f, newPos.y * 0.05f - 10.0f);
+
+        float Pitch = hand.PalmNormal.Pitch + Mathf.Deg2Rad*90;
+        float Roll = hand.PalmNormal.Roll;
+
+        Debug.Log(Pitch.ToString());
+        Pitch = Mathf.Clamp(Pitch, -45 * Mathf.Deg2Rad, 45 * Mathf.Deg2Rad);
+
+        transform.rotation = new Quaternion(-Pitch, 0, Roll, 1);
+    }
+}
