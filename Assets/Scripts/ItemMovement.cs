d58b95d2b16a70bd82544870be1c800fbf1a83d1
Zinccccc <velocity2772@gmail.com>
2013-10-16 20:30:58 +0900

    Item.prefab ItemSpawnPoint ItemMovement 스크립트 추가

---
+++ Assets/Scripts/ItemMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemMovement : MonoBehaviour {
+	
+	private Vector3 startPoint = new Vector3(Random.Range(0, 5), Random.Range(0, 5), 50);
+	private Vector3 speed =  new Vector3(0, 0, 30) ;
+	
+	// Use this for initialization
+	void Start () {
+		
+		//Transform target = GameObject.Find("m346").transform;
+			
+		//transform.position = target.position + new Vector3(0, 0, 50);	
+		transform.position = startPoint;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position-=speed*Time.deltaTime;
+		
+		if(transform.position.z <= -50){
+		Destroy(gameObject);
+		}
+	
+	}
+}
---
+++ Assets/Scripts/ItemSpawnPoint.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawnPoint : MonoBehaviour {
+	
+	public GameObject item;
+	public float min = 0.5f;
+	public float max = 1f;
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("SpawnItem");
+		
+	}
+	
+	IEnumerator SpawnItem(){
+		while(true){
+			float interval = Random.Range(min, max);
+	
+			Instantiate(item, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(interval);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
