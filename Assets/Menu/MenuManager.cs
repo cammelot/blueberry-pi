4acd21ca51bc83667973b48ee0fe08cd2e5f17a9
Cho-Eun-Ji <cammelot@naver.com>
2013-11-08 15:20:02 +0900

    Scene

---
+++ Assets/Menu/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	
+	public GUITexture start;
+	public GUITexture option;
+	public GUITexture end;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(start.HitTest(Input.mousePosition)&&Input.GetMouseButton(0))
+			Application.LoadLevel("a");
+	}
+}
